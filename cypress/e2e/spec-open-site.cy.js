06b94ad1e729c70227bc283ca07dae225fc0dc31
gr413_shiivi <shigaev_1990@mail.ru>
2023-03-02 18:09:11 +0700

    123

--- cypress/e2e/spec-open-site.cy.js
+++ cypress/e2e/spec-open-site.cy.js
@@ -5,12 +5,15 @@ describe('Launches a website', () => {
         cy.get('[data-testid="address-input"]').type('1-й Кирпичный переулок, 17')
         cy.get('[data-testid="address-input"]').type('{enter}')
         cy.get('[data-testid="desktop-location-modal-confirm-button"]').click()
-        // cy.clock().then((clock) => {
-        //     clock.tick(1000)
-        // })
+
         cy.get('[data-testid="search-input"]').type('Макароны')
         cy.get('[data-testid="search-input"]').type('{enter}')
         cy.get('.ChangeLanguage_languageRoot').click()
         cy.get('.ChangeLanguage_dropdown > :nth-child(2)').click()
+        cy.get('.SearchSwitcher_list > :nth-child(2)').click()
+        // cy.clock().then((clock) => {
+        //     clock.tick(1000)
+        // })
+        cy.get('[data-testid="new-header-logo"]').click()
     });
 })
